504ec09afb41147e363c80481bff01346fc538bd
olivierqc1 <olivierturgeon@hotmail.fr>
2025-04-02 22:58:48 -0400

    Create LoadingOverlay.tsx

---
+++ LoadingOverlay.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LoadingOverlay.css';
+
+interface LoadingOverlayProps {
+  message?: string;
+}
+
+const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ message = 'Loading...' }) => {
+  return (
+    <div className="loading-overlay">
+      <div className="loading-spinner"></div>
+      <p className="loading-message">{message}</p>
+    </div>
+  );
+};
+
+export default LoadingOverlay;
