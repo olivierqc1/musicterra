aa5db670f6d21e81a163d901959cb4fe7e76af2e
olivierqc1 <olivierturgeon@hotmail.fr>
2025-05-11 13:49:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // 👈 essentiel pour les chemins relatifs sur Vercel
   plugins: [react()],
   server: {
     port: 3000
