8eb6ad8fb88c63ff3a256472dbffc2eeb49c44da
olivierqc1 <olivierturgeon@hotmail.fr>
2025-05-11 12:29:35 -0400

    Create matrix.ts

---
+++ matrix.ts
@@ -0,0 +1,53 @@
+export const matrix = {
+  "France": {
+    "1970s": {
+      similarCountries: ["Belgium", "Italy"],
+      dominantGenre: "chanson française",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DWYVURwQHUqnN"
+    },
+    "1990s": {
+      similarCountries: ["Canada", "Switzerland"],
+      dominantGenre: "variété pop",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DX4o1oenSJRJd"
+    },
+    "2010s": {
+      similarCountries: ["Belgium", "Morocco"],
+      dominantGenre: "rap francophone",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DX8ymr6UES7vc"
+    }
+  },
+  "Brazil": {
+    "1970s": {
+      similarCountries: ["Argentina", "Colombia"],
+      dominantGenre: "MPB",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DWYtS0z6WXjfd"
+    },
+    "1990s": {
+      similarCountries: ["Mexico", "Chile"],
+      dominantGenre: "axé",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DWZwdG5avllcX"
+    },
+    "2010s": {
+      similarCountries: ["Portugal", "Spain"],
+      dominantGenre: "funk carioca",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DX1ngEVM0lKrb"
+    }
+  },
+  "Japan": {
+    "1970s": {
+      similarCountries: ["South Korea", "China"],
+      dominantGenre: "city pop",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DX1s9knjP51Oa"
+    },
+    "1990s": {
+      similarCountries: ["Taiwan", "Singapore"],
+      dominantGenre: "J-pop",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DX7Gt7Clv3K7z"
+    },
+    "2010s": {
+      similarCountries: ["South Korea", "Thailand"],
+      dominantGenre: "anime soundtracks",
+      playlist: "https://open.spotify.com/playlist/37i9dQZF1DX5Ejj0EkURtP"
+    }
+  }
+};
