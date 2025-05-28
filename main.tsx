27089511b8394c62ede60f82a147fb40d1f29fc5
olivierqc1 <olivierturgeon@hotmail.fr>
2025-05-27 21:26:18 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
-import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './ThemeContext';
+import { AuthProvider } from './AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
