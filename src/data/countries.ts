729d6049cd410021094588e72745eb01b448243a
olivierqc1 <olivierturgeon@hotmail.fr>
2025-08-09 10:54:53 -0400

    premierpsuh

---
+++ src/data/countries.ts
@@ -0,0 +1,65 @@
+export const countries = [
+  {
+    name: 'France',
+    descriptionFr: "Chanson française, électro, rap moderne.",
+    descriptionEn: "Home of chanson, electronic music, and modern rap.",
+    regions: ['Île-de-France', 'Marseille', 'Lyon'],
+    artists: ['Édith Piaf', 'Daft Punk', 'MC Solaar'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWVpjAJGB70vU",
+    image: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Paris_from_the_Tour_Saint-Jacques_2011.jpg"
+  },
+  {
+    name: 'Brazil',
+    descriptionFr: "Samba, bossa nova, MPB, au cœur d'une culture vibrante.",
+    descriptionEn: "Samba, bossa nova, and MPB from a vibrant culture.",
+    regions: ['Rio de Janeiro', 'Bahia', 'São Paulo'],
+    artists: ['Gilberto Gil', 'Caetano Veloso', 'Anitta'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DX7UE7qrnkvsf",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Copacabana_Beach_Rio_de_Janeiro.jpg"
+  },
+  {
+    name: 'Spain',
+    descriptionFr: "Flamenco, musique gitane, et pop moderne.",
+    descriptionEn: "Flamenco, gypsy music, and modern pop.",
+    regions: ['Andalousie', 'Catalogne', 'Madrid'],
+    artists: ['Paco de Lucía', 'Rosalía', 'Manolo Sanlúcar'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWU0ScTcjJBdj",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/db/Alhambra.jpg"
+  },
+  {
+    name: 'Tanzania',
+    descriptionFr: "Bongo Flava, taarab, et rythmes traditionnels de la côte swahilie.",
+    descriptionEn: "Bongo Flava, taarab, and traditional Swahili coast rhythms.",
+    regions: ['Dar es Salaam', 'Zanzibar', 'Arusha'],
+    artists: ['Diamond Platnumz', 'Ali Kiba', 'Lady Jaydee'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWU0ScTcjJBdj", // playlist Tanzanie
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Zanzibar_Stone_Town.jpg"
+  },
+  {
+    name: 'Kenya',
+    descriptionFr: "Afro-pop, genge, benga et musique traditionnelle des hauts plateaux.",
+    descriptionEn: "Afro-pop, genge, benga, and traditional highland music.",
+    regions: ['Nairobi', 'Mombasa', 'Kisumu'],
+    artists: ['Sauti Sol', 'Nyashinski', 'Fadhilee Itulya'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DX9lAKJGxQ0Im", // playlist Kenya
+    image: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Nairobi_skyline.jpg"
+  },
+  {
+    name: 'Uganda',
+    descriptionFr: "Afrobeats, kadongo kamu et fusion afro-électronique.",
+    descriptionEn: "Afrobeats, kadongo kamu, and afro-electronic fusion.",
+    regions: ['Kampala', 'Jinja', 'Gulu'],
+    artists: ['Eddy Kenzo', 'Sheebah Karungi', 'Bebe Cool'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DX3EbcelyrZPd", // playlist Uganda
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Kampala_skyline.jpg"
+  },
+  {
+    name: 'Rwanda',
+    descriptionFr: "Afrobeat, gospel, et musique traditionnelle des collines.",
+    descriptionEn: "Afrobeat, gospel, and traditional hill country music.",
+    regions: ['Kigali', 'Huye', 'Musanze'],
+    artists: ['Meddy', 'The Ben', 'Aline Gahongayire'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWXb9I5xoXLjp", // playlist Rwanda
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Kigali_city_view.jpg"
+  }
+];
---
+++ src/data/genres.ts
@@ -0,0 +1,92 @@
+export const genres = [
+  {
+    name: 'Rock',
+    descriptionFr: "Né dans les années 50 aux USA, influencé par le blues et la country.",
+    descriptionEn: "Born in the 1950s in the USA, influenced by blues and country.",
+    subgenres: ['Progressif', 'Alternatif', 'Psychédélique'],
+    artists: ['The Beatles', 'Led Zeppelin', 'Pink Floyd'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWXRqgorJj26U",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/57/Electric_guitar.jpg"
+  },
+  {
+    name: 'Metal',
+    descriptionFr: "Plus lourd et agressif, popularisé dans les années 70.",
+    descriptionEn: "Heavier and more aggressive, popular since the 1970s.",
+    subgenres: ['Black Metal', 'Heavy Metal', 'Death Metal'],
+    artists: ['Metallica', 'Iron Maiden', 'Black Sabbath'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DX9qNs32fujYe",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/98/Heavy_metal_band.jpg"
+  },
+  {
+    name: 'Reggae',
+    descriptionFr: "Originaire de la Jamaïque, rythmes lents et messages sociaux.",
+    descriptionEn: "Originated in Jamaica, slow rhythms and social messages.",
+    subgenres: ['Roots', 'Dancehall', 'Dub'],
+    artists: ['Bob Marley', 'Peter Tosh', 'Burning Spear'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DXbSbnqxMTGx9",
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Bob-Marley.jpg"
+  },
+  {
+    name: 'Jazz',
+    descriptionFr: "Improvisation née à la Nouvelle-Orléans.",
+    descriptionEn: "Improvisation at its core, born in New Orleans.",
+    subgenres: ['Bebop', 'Swing', 'Free Jazz'],
+    artists: ['Miles Davis', 'John Coltrane', 'Louis Armstrong'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DXbITWG1ZJKYt",
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Jazz_band.jpg"
+  },
+  {
+    name: 'Bongo Flava',
+    descriptionFr: "Genre tanzanien mêlant hip-hop, R&B, et influences swahilies.",
+    descriptionEn: "Tanzanian genre mixing hip-hop, R&B, and Swahili influences.",
+    subgenres: ['Afrobeat Fusion', 'Swahili Pop'],
+    artists: ['Diamond Platnumz', 'Ali Kiba', 'Harmonize'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DXdI8tzTp8Kjk",
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a4/Diamond_Platnumz_live.jpg"
+  },
+  {
+    name: 'Taarab',
+    descriptionFr: "Musique traditionnelle de Zanzibar, mélange arabe, swahili et indien.",
+    descriptionEn: "Traditional music from Zanzibar, blending Arabic, Swahili, and Indian influences.",
+    subgenres: ['Classique', 'Moderne'],
+    artists: ['Bi Kidude', 'Culture Musical Club'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWSmCnE8n7DWH",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5d/Zanzibar_Taarab_performance.jpg"
+  },
+  {
+    name: 'Genge',
+    descriptionFr: "Hip-hop kenyan avec paroles en sheng et rythmes locaux.",
+    descriptionEn: "Kenyan hip-hop with sheng lyrics and local rhythms.",
+    subgenres: ['Genge Tone', 'Hip-hop Fusion'],
+    artists: ['Jua Cali', 'Nonini'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWYkaDif7Ztbp",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/82/Jua_Cali_performance.jpg"
+  },
+  {
+    name: 'Benga',
+    descriptionFr: "Musique dansante kenyane, guitare rapide et rythme entraînant.",
+    descriptionEn: "Danceable Kenyan music with fast guitar and upbeat rhythm.",
+    subgenres: ['Classic Benga', 'Modern Benga'],
+    artists: ['D.O. Misiani', 'Suzanna Owiyo'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DXcOZ52XmE7XQ",
+    image: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Benga_band_performance.jpg"
+  },
+  {
+    name: 'Kadongo Kamu',
+    descriptionFr: "Musique ougandaise à une guitare, souvent avec des paroles narratives.",
+    descriptionEn: "Ugandan one-guitar music, often with narrative lyrics.",
+    subgenres: ['Traditionnel', 'Moderne'],
+    artists: ['Paul Kafeero', 'Herman Basudde'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DWUJcOa0BwVX5",
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Uganda_traditional_music.jpg"
+  },
+  {
+    name: 'Rwandan Afrobeat',
+    descriptionFr: "Fusion afrobeat, gospel et pop rwandaise contemporaine.",
+    descriptionEn: "Afrobeat fusion, gospel, and contemporary Rwandan pop.",
+    subgenres: ['Afro-Gospel', 'Urban Pop'],
+    artists: ['Meddy', 'The Ben'],
+    spotify: "https://open.spotify.com/playlist/37i9dQZF1DX7dZc2QeWvZQ",
+    image: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Meddy_Rwanda.jpg"
+  }
+];
