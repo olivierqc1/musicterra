5816dcc33344863dbfa4b760cb4d288e468b7150
olivierqc1 <olivierturgeon@hotmail.fr>
2025-09-18 15:39:17 -0400

    pushmain

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+// Styles globaux
+import "./index.css";
+
+// Auth
 import { AuthProvider } from "./context/AuthContext";
 import AuthGate from "./components/AuthGate";
-import "./index.css";
+
+// Debug
+import DebugPanel from "./components/DebugPanel";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
       <AuthGate>
         <App />
+        {/* Debug visible uniquement en dev/test */}
+        {true && <DebugPanel />}
       </AuthGate>
     </AuthProvider>
   </React.StrictMode>
