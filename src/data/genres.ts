e682c8b6efcf195d55a377ac0bba1389df8ebc92
olivierqc1 <olivierturgeon@hotmail.fr>
2025-10-02 21:01:28 -0400

    Update genres.ts

--- src/data/genres.ts
+++ src/data/genres.ts
@@ -242,6 +242,236 @@ export const genres: Item[] = [
   spotify: "https://open.spotify.com/playlist/5dSNRnhAMz7Uw3io5UUEQ2?si=_rrRrOkySIWiDhTa4oV7AA",
   image: "https://upload.wikimedia.org/wikipedia/commons/5/5d/Panama_City_urban.jpg"
 },
+  // Argentina
+{
+  name: 'Tango',
+  descriptionFr: "Danse/genre iconique de Buenos Aires, bandonéon et nostalgie.",
+  descriptionEn: "Iconic Buenos Aires dance/music with bandoneon and nostalgia.",
+  subgenres: ['Tango Nuevo', 'Milonga'],
+  artists: ['Astor Piazzolla', 'Carlos Gardel'],
+  spotify:"https://open.spotify.com/playlist/0sAAYyQQemt7UD1rbICQJo?si=WcEpXoEVRf2fFmH7iSrWZw",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/52/Street_tango_Buenos_Aires.jpg"
+},
+{
+  name: 'Rock Nacional (AR)',
+  descriptionFr: "Rock argentin en espagnol, identitaire et populaire.",
+  descriptionEn: "Argentinian Spanish-language rock, identity-defining and popular.",
+  subgenres: [],
+  artists: ['Soda Stereo', 'Charly García'],
+  spotify:"https://open.spotify.com/playlist/67e8YlkxAwMsoAsqXVoBCh?si=UZOPgedETje2mFoYMmBteg&pi=tizXF6zqQXmTc",
+  image: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Electric_guitar_closeup.jpg"
+},
+
+// Chile
+{
+  name: 'Cueca',
+  descriptionFr: "Danse/genre national chilien, guitare, harpe et chant dialogué.",
+  descriptionEn: "Chilean national dance/music with guitar, harp and call-response.",
+  subgenres: [],
+  artists: ['Violeta Parra'],
+  spotify:"https://open.spotify.com/playlist/3d2xkfoedWPUtNCkql58QU?si=3u8QRFdSQryWLMwzIfaZew",
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/55/Cueca_chilena.jpg"
+},
+{
+  name: 'Nueva Canción',
+  descriptionFr: "Mouvement folk engagé du Cône Sud (années 60-70).",
+  descriptionEn: "Politically-engaged folk movement in the Southern Cone (60s-70s).",
+  subgenres: [],
+  artists: ['Víctor Jara', 'Inti-Illimani'],
+  spotify:"https://open.spotify.com/playlist/37i9dQZF1EIcKzfRPMeD3p?si=pV-ignbnS6aqeqh-et9R1Q",
+  image: "https://upload.wikimedia.org/wikipedia/commons/1/10/Charango.jpg"
+},
+
+// Colombia
+{
+  name: 'Cumbia (CO)',
+  descriptionFr: "Genre côtier colombien, percussions, flûtes de millo et danse circulaire.",
+  descriptionEn: "Colombian coastal genre with percussion, flautas de millo and circle dance.",
+  subgenres: [],
+  artists: ['Totó la Momposina', 'Joe Arroyo'],
+  spotify:"https://open.spotify.com/playlist/0h3mUr2mwCoAlVDbZwFC2s?si=ZIuJIiABTZK5eDqyxgOEvA",
+  image: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Cumbia_Colombia.jpg"
+},
+{
+  name: 'Vallenato',
+  descriptionFr: "Musique de l’accordéon des Caraïbes colombiennes.",
+  descriptionEn: "Accordion-driven music from Colombia’s Caribbean region.",
+  subgenres: [],
+  artists: ['Carlos Vives', 'Diomedes Díaz'],
+  spotify:"https://open.spotify.com/playlist/4hPws0Egz0eicp6DvEcdyf?si=9pnCJtHTT2iV0Xny4UlE3g&pi=YEU4QBFGR_-nn",
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Accordion.jpg"
+},
+{
+  name: 'Champeta',
+  descriptionFr: "Genre urbain afro-caribéen de Cartagena.",
+  descriptionEn: "Afro-Caribbean urban genre from Cartagena.",
+  subgenres: [],
+  artists: ['Charles King'],
+  spotify:"https://open.spotify.com/playlist/3WnrSBX3vfya9CLyID4pmX?si=3WA1RhVoRRm2YJzsQi88hA",
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/0f/DJ_turntable.jpg"
+},
+
+// Peru
+{
+  name: 'Huayno',
+  descriptionFr: "Musique andine (voix hautes, charango, quena).",
+  descriptionEn: "Andean music (high vocals, charango, quena).",
+  subgenres: [],
+  artists: ['Flor Pucarina'],
+  spotify:"https://open.spotify.com/playlist/37i9dQZF1DX0dskKWpx62P?si=eqDWNDruTS6s6zCY7qiGPw",
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/0a/Charango_bolivia.jpg"
+},
+{
+  name: 'Chicha (PE)',
+  descriptionFr: "Cumbia andine urbaine, guitares électriques et synthés.",
+  descriptionEn: "Urban Andean cumbia with electric guitars and synths.",
+  subgenres: [],
+  artists: ['Los Mirlos', 'Los Shapis'],
+  spotify:"https://open.spotify.com/playlist/2sWXv2tVWo3Gi19HcxL9d4?si=7Xa2eW06RZOnMXqIZlKYJQ",
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/52/Electric_guitar.jpg"
+},
+{
+  name: 'Festejo',
+  descriptionFr: "Genre afro-péruvien festif de la côte centrale.",
+  descriptionEn: "Festive Afro-Peruvian genre from the central coast.",
+  subgenres: [],
+  artists: ['Eva Ayllón', 'Susana Baca'],
+  spotify:"https://open.spotify.com/playlist/3iWwh0cIjpn6344d4iiGrn?si=W7M5Seo0TmS4N26tivfLlA",
+  image: "https://upload.wikimedia.org/wikipedia/commons/7/78/Cajon_peruano.jpg"
+},
+
+// Bolivia
+{
+  name: 'Morenada',
+  descriptionFr: "Danse/rythme du plateau andin, grosses caisses et matracas.",
+  descriptionEn: "Highland Andean dance/rhythm with bass drums and rattles.",
+  subgenres: [],
+  artists: ['Los Kjarkas'],
+  spotify:"https://open.spotify.com/playlist/5F6eeNiqNLD6cdgJ7S9GWJ?si=zHJsipm9R0aezqnifRCeYQ",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/97/Diablada_Oruro.jpg"
+},
+{
+  name: 'Caporales',
+  descriptionFr: "Danse scénique bolivienne, tambours et sifflets.",
+  descriptionEn: "Bolivian stage dance with drums and whistles.",
+  subgenres: [],
+  artists: [],
+  spotify:"https://open.spotify.com/playlist/2DySCtsLZVnU86lDJn96jh?si=CIX6bWn3S0a5hGnpWEpVmw",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/97/Diablada_Oruro.jpg"
+},
+
+// Ecuador
+{
+  name: 'Pasillo (EC)',
+  descriptionFr: "Ballade romantique équatorienne en 3/4.",
+  descriptionEn: "Romantic Ecuadorian ballad in 3/4.",
+  subgenres: [],
+  artists: ['Julio Jaramillo'],
+  spotify:"https://open.spotify.com/playlist/7CDqiIvzkhUmxGa8djpFWG?si=JyY6odNlSKWE4ULBxyashA",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/49/Acoustic_guitar_nylon.jpg"
+},
+{
+  name: 'Bomba del Chota',
+  descriptionFr: "Musique afro-équatorienne à grosse caisse, danse communautaire.",
+  descriptionEn: "Afro-Ecuadorian music with bass drum, communal dance.",
+  subgenres: [],
+  artists: [],
+  spotify:"https://open.spotify.com/playlist/5hPl8oMA2v3MINqHs849IR?si=k8Ku7fcVQh-3DiTtjbelJQ&pi=quPlehb6S_CaK",
+  image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Drum_bass.jpg"
+},
+
+// Paraguay
+{
+  name: 'Guarania',
+  descriptionFr: "Genre lent et lyrique, emblème du Paraguay.",
+  descriptionEn: "Slow, lyrical Paraguayan genre.",
+  subgenres: [],
+  artists: ['José Asunción Flores'],
+  spotify:"https://open.spotify.com/playlist/7s0cQHnZTKueykafCY23Ji?si=oUTt-cCVR7mQle34TP3T4g&pi=DCTuDG4xSpenr",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Paraguayan_harp.jpg"
+},
+{
+  name: 'Polka Paraguaya',
+  descriptionFr: "Danse vive en 6/8, harpe paraguayenne.",
+  descriptionEn: "Lively 6/8 dance with Paraguayan harp.",
+  subgenres: [],
+  artists: ['Luis Alberto del Paraná'],
+  spotify:"https://open.spotify.com/playlist/1jdYkR6P4UP6etoDImX5fB?si=9MyMZToBSMSxYpCdR44TZA",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Paraguayan_harp.jpg"
+},
+
+// Uruguay
+{
+  name: 'Candombe (UY)',
+  descriptionFr: "Rythme afro-uruguayen aux tambours (tambores) de Montevideo.",
+  descriptionEn: "Afro-Uruguayan drum rhythm from Montevideo.",
+  subgenres: [],
+  artists: ['Rubén Rada'],
+  spotify:"https://open.spotify.com/playlist/3CbJCgy9y1pFcbhJB2gnji?si=WLQGQz5MQSquLYb6FIKftg",
+  image: "https://upload.wikimedia.org/wikipedia/commons/1/19/Candombe_Uruguay.jpg"
+},
+{
+  name: 'Murga (UY)',
+  descriptionFr: "Théâtre musical choral carnavalesque.",
+  descriptionEn: "Carnival choral musical theatre.",
+  subgenres: [],
+  artists: [],
+  spotify:"https://open.spotify.com/playlist/37i9dQZF1DX17W5sS8YJkb?si=PKHGbfZvSkCq-L0paL2PJg&pi=6vdEukZLQH2qf",
+  image: "https://upload.wikimedia.org/wikipedia/commons/1/1d/Choir_singing.jpg"
+},
+
+// Venezuela
+{
+  name: 'Joropo',
+  descriptionFr: "Genre des Llanos (harpe, cuatro, maracas), danse rapide.",
+  descriptionEn: "Plains genre (harp, cuatro, maracas), fast dance.",
+  subgenres: [],
+  artists: ['Simón Díaz'],
+  spotify:"https://open.spotify.com/playlist/1z93667X0oCSAd6AlNeboq?si=1Bvg6IIWRzyGnLsKCstqxA",
+  image: "https://upload.wikimedia.org/wikipedia/commons/8/86/Cuatro_venezolano.jpg"
+},
+{
+  name: 'Gaita Zuliana',
+  descriptionFr: "Musique festive de Maracaibo (Noël), tambora et chœurs.",
+  descriptionEn: "Festive Maracaibo (Christmas) music with tambora and choirs.",
+  subgenres: [],
+  artists: ['Guaco'],
+  spotify:"https://open.spotify.com/playlist/37i9dQZF1EIcNVBZzdriPe?si=eGcSak9hQg2NgITTyd-9uA",
+  image: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Tambora_drum.jpg"
+},
+
+// Guyana
+{
+  name: 'Chutney (GY)',
+  descriptionFr: "Genre indo-caribéen avec harmonium, dholak et soca.",
+  descriptionEn: "Indo-Caribbean genre with harmonium, dholak and soca.",
+  subgenres: [],
+  artists: ['Terry Gajraj'],
+  spotify:"https://open.spotify.com/playlist/4wTwN9B3y7YaCotth5EPHl?si=qyny369qQdSlg3ctATc46Q",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Harmonium.jpg"
+},
+
+// Suriname
+{
+  name: 'Kaseko',
+  descriptionFr: "Musique créole surinamaise (tambours, cuivres), danse.",
+  descriptionEn: "Creole Surinamese music (drums, horns), dance.",
+  subgenres: [],
+  artists: ['Lieve Hugo'],
+  spotify:"https://open.spotify.com/playlist/2Bv4dNyJCGb4T6olVXeep7?si=0IA9j4qNRvu_FjWziaFvGg",
+  image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Brass_band.jpg"
+},
+
+// French Guiana
+{
+  name: 'Kasékò',
+  descriptionFr: "Genre créole guyanais, tambours et call-response.",
+  descriptionEn: "French Guianese Creole genre with drums and call-response.",
+  subgenres: [],
+  artists: [],
+  spotify:"https://open.spotify.com/playlist/37i9dQZF1EIeOlAvJLiYgJ?si=mXL9Ol6PQeOA3UmTLQOu0g",
+  image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Drums_set.jpg"
+},
 ];
 
 
